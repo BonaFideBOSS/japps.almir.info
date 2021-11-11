2283d30262df892b09de3d9bf5d4960356bd4188
BONAFIDE BOSS <amaanalmir@gmail.com>
2021-11-10 21:12:26 -0500

    update

--- app/js/currency-converter.js
+++ app/js/currency-converter.js
@@ -1,10 +1,13 @@
-const dropList = document.querySelectorAll("form select")
 const currency_one = document.getElementById('currency_one');
 const currency_two = document.getElementById('currency_two');
 const amount_one = document.getElementById('amount_one');
 const amount_two = document.getElementById('amount_two');
 const rate = document.getElementById('rate');
 const userrate = document.getElementById('userrate');
+const amountform = document.querySelectorAll('form');
+const countrycontainer = document.getElementsByClassName('country-container');
+const flag_one = document.getElementById('flag_one');
+const flag_two = document.getElementById('flag_two');
 const country_one = document.getElementById('country_one');
 const country_two = document.getElementById('country_two');
 const exchange = document.getElementById('exchange');
@@ -26,6 +29,12 @@ function calculate() {
     const value_two = currency_two.value;
     const countryone = currency_one.options[currency_one.selectedIndex].text;
     const countrytwo = currency_two.options[currency_two.selectedIndex].text;
+    const flagone = 'https://flagcdn.com/w2560/' + value_one.slice(0, -1).toLowerCase() + '.png';
+    const flagtwo = 'https://flagcdn.com/w2560/' + value_two.slice(0, -1).toLowerCase() + '.png';
+    console.log(flagone);
+    console.log(flagtwo);
+    console.log(amountform.length)
+
 
 
     fetch(`https://api.exchangerate.host/latest/?base=${value_one}`)
@@ -46,10 +55,22 @@ function calculate() {
             currentTime = hours + ":" + minutes + " " + ampm;
             if (value_one != '' && value_two != '') {
                 rate.style.display = "block";
+                if ($(window).width() < 767) {
+                    var j;
+                    for (j = 0; j < amountform.length; j++) {
+                        amountform[j].style.marginBottom = ".1rem";
+                    }
+                }
+                var i;
+                for (i = 0; i < countrycontainer.length; i++) {
+                    countrycontainer[i].style.display = "block";
+                }
                 userrate.style.display = "block";
                 rate.innerHTML = `1 ${value_one} equals <span>${current_rate} ${value_two}</span> <footer class="font-monospace">${month} ${date}, ${year} - ${currentTime}</footer>`
-                country_one.innerHTML = countryone
-                country_two.innerHTML = countrytwo
+                flag_one.src = flagone;
+                flag_two.src = flagtwo;
+                country_one.innerHTML = countryone;
+                country_two.innerHTML = countrytwo;
             }
 
             if (amount_two === document.activeElement) {
